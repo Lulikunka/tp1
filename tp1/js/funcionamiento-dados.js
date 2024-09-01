85d71c28a885a922b2584cc2721074c139b56583
Lulikunka <lulikunka@gmail.com>
2024-09-01 17:41:39 -0300

    Add files via upload

---
+++ tp1/js/funcionamiento-dados.js
@@ -0,0 +1,4 @@
+//Acá van las imágenes de los dados para c/cara de los dados
+//También van las imágenes al final para mostrar si el jugador 1 ha ganado o perdido
+//Para mostrar las imágenes puedo usar una función, porque tambíen la utilizo en los otros programas
+
