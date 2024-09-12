2f0c521a386cc71fad9272397349a72c3ce79f35
Maligna.sospechosa <lulikunka@gmail.com>
2024-09-12 13:14:45 -0300

    Add files via upload

--- js/funcionamiento-cartas.js
+++ js/funcionamiento-cartas.js
@@ -16,11 +16,11 @@ let caratulaCartasImg=document.querySelector('#imgCaratulaCartas');//Capturo la 
 let inicio=document.querySelector('.comenzarJuego');//Capturo el botón para habilitar el inicio de juego
 
 //Selección de avatar: variables para la función
-let avatarRen=document.querySelector('#avatarRen');//Para aplicar interactividad
-let avatarRenSeleccion=document.querySelector('#ren');//Operar con avatar
-let avatarNana=document.querySelector('#avatarNana');//Para aplicar interactividad
-let avatarNanaSeleccion=document.querySelector('#nana');//Operar con avatar
-let botonAvatar=document.querySelector('#submitAvatar');//Para aceptar el avatar
+let avatarRen=document.querySelector('#avatarRen');//Para aplicar interactividad en imagem
+let avatarRenSeleccion=document.querySelector('#ren');//Operar con label avatar
+let avatarNana=document.querySelector('#avatarNana');//Para aplicar interactividad en imagem
+let avatarNanaSeleccion=document.querySelector('#nana');//perar con label avatar
+let botonAvatar=document.querySelector('#submitAvatar');//Para aceptar el avatar y nombre
 
 //Inicio perfil de jugador. Capturo las imágenes para mostrar una y esconder la otra, dependiendo del avatar seleccionado.
 let nombre;
@@ -30,19 +30,18 @@ let perfilNana=document.querySelector('#perfilNana');
 let divDeJuego=document.querySelector('.divJuegoDeCartas');//Capturo el div para que aparezca el div del juego
 let divDeAvatar=document.querySelector('#registrarseCartasAvatar');//Capturo el div para que permita seleccionar avatar
 let divCartasNombre=document.querySelector('#registrarseCartasNombre');//Capturo el div para ingresar el nombre
-//let divNombreJugador=document.querySelector('');//Capturo el div para que el jugador introduzca su nombre
+let nombreJugador=document.querySelector('#nombreJugadorUno');//Capturo el parrafo para que aparezca el nombre del j1
 //Declaro ambos jugadores como arrays para asignarle las cartas
 let jugadorUno=[];
 let jugadorDos=[];
 
 let mostrarCartasJugadorUno; //Variable para mostrar los valores asignados
-
 let mostrarCartasJugadorDos;
+
+let sectionApuesta=document.querySelector('#sectionApuesta');
 
 
 let comenzarPartidaButton=document.querySelector('.botonComenzarPartida'); //Capturo al botón para comenzar la partida y qse repartan las cartas
-
-let apuesta;
 
 inicio.addEventListener('click', ()=>{
     caratulaCartasH3.style.display = 'none';
@@ -100,30 +99,44 @@ interactividadAvatar();
 botonAvatar.addEventListener('click', e=> {//Creo el evento para el botón de selección
     // Uso preventDefault para que no se recargue la página
     e.preventDefault();
+    ingrasarNombre();
     elegirAvatar();  // Llamo a la función elegirAvatar aquí
 });
 
 // Función para elegir el avatar, generar el hover y el if para la selección
+let ingrasarNombre=()=>{
+    let nombrePersona = document.querySelector('#nombre').value;
+    // Valido que el nombre no esté vacío
+    if (nombrePersona == '') {
+        alert('El nombre no puede estar vacío');
+        return false;
+    }
+    perfilJugador.nombre=nombrePersona;
+    nombreJugador.innerHTML=nombrePersona
+}
 let elegirAvatar = () => {
     if(avatarRenSeleccion.checked == true){
         divDeAvatar.style.display='none';
         divDeAvatar.style.visibility='hidden';
         perfilNana.style.display='none';
+        divCartasNombre.style.display='none';
         divDeJuego.style.display='block';
         divDeJuego.style.visibility='visible';
-        perfilJugador.avatar= avatarRen;
+        perfilJugador.avatar= avatarRen;//agrego la imagen al objeto del PERFIL DEL JUGADOR para mostrarloluego en la tabla
     } else if(avatarNanaSeleccion.checked == true){
         divDeAvatar.style.display='none';
         divDeAvatar.style.visibility='hidden';
         perfilRen.style.display='none';
+        divCartasNombre.style.display='none';
         divDeJuego.style.display='block';
         divDeJuego.style.visibility='visible';
-        perfilJugador.avatar= avatarNana;
+        perfilJugador.avatar= avatarNana;//agrego la imagen al objeto del PERFIL DEL JUGADOR para mostrarloluego en la tabla
+    } else{
+        alert('Tiene que elegir un avatar');
+        return false;
     }
+    
 }
-
-elegirAvatar();
-
 
 let crearMazo = () => {//Función para crear mazo uniendo numeros, palos y valores
     for (let i = 0; i < palos.length; i++) {
@@ -155,6 +168,7 @@ comenzarPartidaButton.addEventListener('click', ()=> {
     setTimeout(comenzarJuego, 500);
     comenzarPartidaButton.style.display='none';
 });
+
 let comenzarJuego=()=>{
     jugadorUno=[];
     for(let i = 0; i < 3; i++){//reparte las cartas p.j1
@@ -164,7 +178,6 @@ let comenzarJuego=()=>{
     }
     mostrarCartasJugadorUno=document.querySelector('#ulJugadorUno');
     mostrarCartasJugadorUno.innerHTML = ''; // Limpia la lista antes de agregar las nuevas cartas.
-
     for (let i = 0; i < jugadorUno.length; i++) { //recorro el nuevp array del jugador para mostrar sus cartas
         let carta = jugadorUno[i];
         mostrarCartasJugadorUno.innerHTML += `<li><img src="${carta.imagen}" alt="${carta.numero} de ${carta.palo}"></li>`;
@@ -177,7 +190,6 @@ let comenzarJuego=()=>{
         jugadorDos.push(mazo[azar]);
         mazo.splice(azar,1);
     }
-    //console.log(jugadorDos) //Cartas repartidas exitosamente. Comprobarlo en la consola
     mostrarCartasJugadorDos=document.querySelector('#ulJugadorDos');
     mostrarCartasJugadorDos.innerHTML = ''; // Limpia la lista antes de agregar las nuevas cartas, PERO HAY QUE PONERLO EN OTRO LADO PORQUE MARCA NaN
 
@@ -186,8 +198,9 @@ let comenzarJuego=()=>{
         
         mostrarCartasJugadorDos.innerHTML += `<li><img id="carta-0" src="imagenes/carta0.png" alt="Carta al revés"></li>`;
     }
-    capturarCartas()
+    capturarCartas()//Capturo las cartas para poder manejar la elección del usuario
     interactividadCartas()
+    
 }
 
 let capturarCartas=()=>{//Capturo los elementos para poder crear la interactividad de las cartas
@@ -202,15 +215,28 @@ let capturarCartas=()=>{//Capturo los elementos para poder crear la interactivid
     }
 
 
-let interactividadCartas = () => {
-    for(let i=0; i<jugadorUno.length;i++){
+let interactividadCartas = () => {//Permite seleccionar la carta
+    for(let i=0; i<jugadorUno.length;i++){//Recorro el jugador uno para poder generar la interactividad con cada carta, dependiendo de su posición
         console.log(jugadorUno[i].posicion);
-        jugadorUno[i].posicion.addEventListener('click', ()=>{
-        jugadorUno[i].posicion.style.border = '.3em solid rgb(221, 202, 33)';
+        jugadorUno[i].posicion.addEventListener('click', ()=>{//Agrego el evento para ue sea posible
+        jugadorUno[i].posicion.style.border = '.2em solid rgb(221, 202, 33)';
+        jugadorUno[i].posicion.classList.add=('cartaElegida1');//Le agrego una clase para poder manejar luego su visibilidad
+
+        let cartaElegida=document.querySelector('#cartaElegida1');
+        cartaElegida.style.display='none';
+        apuesta()
         })
     }
 }
 
+let apuesta=()=>{
+    sectionApuesta.style.display='block';
+    sectionApuesta.style.visibility='visible';
+}
+
+let partidacompleta=()=>{
+    
+}
 //Comienza el juego:: función de interactividad con las cartas
 
 
